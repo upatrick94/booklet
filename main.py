f3c24153dee9e82ddfba308dd1f3a7f1c53ad592
ursu_patrick9 <patrick.ursu@stud.ubbcluj.ro>
2025-10-16 12:14:12 +0300

    .

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from stats import count_words,count_characters
+from stats import count_words,count_characters,sorted_count_characters
+import sys
 
 def get_book_test(filePath: str):
     with open(filePath) as f:
@@ -6,10 +7,19 @@ def get_book_test(filePath: str):
     return fileContents
 
 def main():
-    book_text = get_book_test("books/frankenstein.txt")
+    if(len(sys.argv) < 2):
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_text = get_book_test(sys.argv[1])
     num_words = count_words(book_text)
-    num_characters = count_characters(book_text)
+    num_characters = sorted_count_characters(book_text)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_characters)
+    print("--------- Character Count -------")
+    for character in num_characters:
+        print(f"{character["char"]}: {character["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -13,8 +13,17 @@ def count_characters(text: str):
                 counter[char] = 1
     return counter
 
+def sort_by_number_of_appearances(items):
+    return items["num"]
 
 def sorted_count_characters(text: str):
     counter = count_characters(text)
+    list =[]
+    for key in counter:
+        list.append({"char": key, "num": counter[key]})
+    list.sort(reverse=True, key=sort_by_number_of_appearances)
+    return list
+    
+
 
 
